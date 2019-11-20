d0ee0152b496b088f0ad1c9c9157cccfa37cc091
Ryan Goldstein <ryan@ryanisaacg.com>
2019-11-20 16:21:45 -0500

    Add a TODO for clean up of resources

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // TODO: should there be a vertex-safe way of inserting stuff into the VBO?
 // TODO: validate vec and matrix dimensions
+// TODO: resource clean up
 #[derive(Debug)]
 pub struct GolemError();
 mod context;
